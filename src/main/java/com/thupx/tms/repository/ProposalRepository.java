2da6946b4ebedb64ffdb3e422a8d625a2bae0a3a
Thu Phạm <xuanthu.ptit@gmail.com>
2022-07-19 08:52:15 +0700

    fix

--- src/main/java/com/thupx/tms/repository/ProposalRepository.java
+++ src/main/java/com/thupx/tms/repository/ProposalRepository.java
@@ -23,14 +23,7 @@ public interface ProposalRepository extends JpaRepository<Proposal, Long> {
 	List<Proposal> findByStatusDateBetween(@Param("statusChart") Boolean statusChart, @Param("startDate") ZonedDateTime startDate, @Param("endDate") ZonedDateTime endDate);
 	 
 	
-	@Query(value = "SELECT * FROM proposal as p, jhi_user as u, hospital_department as h WHERE p.user_extra_user_id = u.id and p.hospital_department_id = h.id and"
-			+ " (UPPER(p.content_proposal) LIKE UPPER(CONCAT('%', TRIM(:search), '%')) or"
-			+ " UPPER(p.current_progress_name) LIKE UPPER(CONCAT('%', TRIM(:search), '%')) or"
-			+ " UPPER(p.note) LIKE UPPER(CONCAT('%', TRIM(:search), '%')) or"
-			+ " (CASE WHEN isnumeric(TRIM(:search)) = true THEN p.id = CAST(TRIM(:search) AS INT) END) or"
-			+ " UPPER(u.first_name) LIKE UPPER(CONCAT('%', TRIM(:search), '%')) or"
-			+ " UPPER(u.last_name) LIKE UPPER(CONCAT('%', TRIM(:search), '%')) or"
-			+ " UPPER(h.hospital_department_name) LIKE UPPER(CONCAT('%', TRIM(:search), '%')))", nativeQuery = true)
+	@Query(value = "SELECT * FROM proposal as p, jhi_user as u, hospital_department as h WHERE p.user_extra_user_id = u.id and p.hospital_department_id = h.id and (UPPER(p.content_proposal) LIKE UPPER(CONCAT('%', TRIM(:search), '%')) or UPPER(p.current_progress_name) LIKE UPPER(CONCAT('%', TRIM(:search), '%')) or UPPER(p.note) LIKE UPPER(CONCAT('%', TRIM(:search), '%')) or (CASE WHEN isnumeric(TRIM(:search)) = true THEN p.id = CAST(TRIM(:search) AS INT) END) or UPPER(u.first_name) LIKE UPPER(CONCAT('%', TRIM(:search), '%')) or UPPER(u.last_name) LIKE UPPER(CONCAT('%', TRIM(:search), '%')) or UPPER(h.hospital_department_name) LIKE UPPER(CONCAT('%', TRIM(:search), '%')))", nativeQuery = true)
 	Page<Proposal> findAll(Pageable pageable,@Param("search") String search);
 	
 	@Query(value = "SELECT * FROM proposal as p, jhi_user as u, hospital_department as h WHERE p.user_extra_user_id = u.id and p.hospital_department_id = h.id and p.user_extra_user_id=:user_extra_user_id and"
